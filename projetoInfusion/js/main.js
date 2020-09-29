cf97ca74462e294d18d93d07435559fadad54247
Guilherme Francisco <guilhermefranciscoav@gmail.com>
2020-09-28 21:57:30 -0300

    Initial commit

---
+++ projetoInfusion/js/main.js
@@ -0,0 +1,29 @@
+(function(){
+	var $body = document.querySelector("body");
+	$body.classList.remove("no-js");
+	$body.classList.add("js");
+
+	var menu = new Menu({
+		container: ".header__nav",
+		toggleBtn: ".header__btnMenu",
+		widthEnabled: 1024
+	});
+
+	//carousel
+
+	var carouselImgs = new Carousel({
+		container: ".laptop-slider .slideshow",
+		itens: "figure",
+		btnPrev: ".prev",
+		btnNext: ".next"
+	});
+
+	//quotes
+
+	var carouselQuotes = new Carousel({
+		container: ".quote-slideshow",
+		itens: "figure",
+		btnPrev: ".prev",
+		btnNext: ".next"
+	});
+})();
